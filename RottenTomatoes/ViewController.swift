953958ac941012769cb33a7fccfa5b9e6deb98cf
jack li <sli@zendesk.com>
2015-02-05 16:22:25 -0800

    commit inital code

--- RottenTomatoes/ViewController.swift
+++ RottenTomatoes/ViewController.swift
@@ -8,17 +8,90 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies = NSArray()
+    var moviesData = NSDictionary()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        tableView.dataSource=self
+        tableView.delegate = self
+        
+        
+        
+        var url = NSURL(string: "http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/top_rentals.json?apikey=dagqdghwaq3e3mxyrp7kmmj5&limit=20&country=us")!
+        
+        var request = NSURLRequest(URL: url)
+     
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            
+            var responseDictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as NSDictionary
+            
+          
+        
+            self.movies = responseDictionary["movies"] as NSArray
+            
+            self.tableView.reloadData()
+            
+            
+            
+          // println("response: \(self.movies)")
+            
+            
+            
+            println(self.movies.count)
+            
+             self.moviesData = responseDictionary
+            
+            
+        }
+        
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier("movieCell") as MovieViewCell
+        
+        //var cell = UITableViewCell()
+        println(indexPath.row)
+        let json = JSON(moviesData["movies"]!)
+        // println(json)
+        println(json[indexPath.row] )
+        //println (movies[indexPath.row])
+        
+        
+        // println (movies[indexPath.row])
+
+        
+      //  cell.textLabel?.text = NSString(format: "section %ld, row %ld, title %s ", indexPath.section, indexPath.row)
+        cell.textLabel?.text = json[indexPath.row]["title"].string
+        return cell
+    }
+    
+   
 
 
 }
